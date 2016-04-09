4dc5f594467dc6a722ebbdf524fa2902bbb3adee
NickMountjoy <nick.mountjoy98@gmail.com>
2016-04-09 12:07:39 +0100

    Game starts at 0

---
+++ spec/GameSpec.js
@@ -0,0 +1,28 @@
+describe("Game", function(){
+  var game;
+  var frame;
+
+  beforeEach(function(){
+    game = new Game();
+    frame = 'new Frame';
+  });
+
+  describe('scoring', function(){
+    it('starts the game at 0', function(){
+      expect(game.getScore()).toEqual(0);
+    });
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+
+})
---
+++ src/Game.js
@@ -0,0 +1,13 @@
+function Game() {
+  this.roll = 0;
+  this.frame = 0;
+  this.frames = []
+  // for(var i = 0; i < 10; i++) {
+  //   this.frames.push(new Frame());
+  // }
+}
+
+Game.prototype.getScore = function() {
+  var total = 0;
+  return total;
+};
